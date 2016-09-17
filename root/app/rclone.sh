f9ef7c04bba7f5df12cfae53e9bce4100e365b08
Thomas Tynor <tynor@hotmail.com>
2016-09-17 19:43:00 +0200

    shouldn't be necessary

--- root/app/rclone.sh
+++ root/app/rclone.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv sh
 
 echo "Running => rclone sync /config AmazonEncrypted:/"
-rclone --config "/config/.rclone.conf" sync /config AmazonEncrypted:/
+rclone sync /config AmazonEncrypted:/
 echo "rclone done!"
