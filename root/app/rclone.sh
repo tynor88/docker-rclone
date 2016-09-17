f47d1269f4a5b9bd89fe7f934d8083d8e1f94e81
Thomas Tynor <tynor@hotmail.com>
2016-09-17 19:58:58 +0200

    fix

--- root/app/rclone.sh
+++ root/app/rclone.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/with-contenv sh
 
-if [ -z "{$REMOTE_SYNC}" ]; then
+if [ -z "$REMOTE_SYNC" ]; then
  echo "REMOTE_SYNC environment variable was not passed to the container."
  exit 1
 else
- echo "Running => rclone sync /config {$REMOTE_SYNC}:/"
- rclone sync /config ${REMOTE_SYNC}:/
+ echo "Running => rclone --config \"/config/.rclone.conf\" sync /config $REMOTE_SYNC:/"
+ rclone --config "/config/.rclone.conf" sync /config $REMOTE_SYNC:/
  echo "rclone done!"
 fi
