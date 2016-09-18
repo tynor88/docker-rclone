dc4c40a48c99f4d080d7ecc27018be487f61535c
Thomas Tynor <tynor@hotmail.com>
2016-09-18 08:45:16 +0200

    add flock to script rather than cron

--- root/app/rclone.sh
+++ root/app/rclone.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/with-contenv sh
 
-if [ -z "$REMOTE_SYNC" ]; then
- echo "Error: REMOTE_SYNC environment variable was not passed to the container."
- exit 1
-else
- synccmd="rclone --config "/config/.rclone.conf" sync /data $REMOTE_SYNC:/"
- echo "Executing => $synccmd"
- eval "$synccmd"
-fi
+(
+  if [ -z "$REMOTE_SYNC" ]; then
+    echo "Error: REMOTE_SYNC environment variable was not passed to the container."
+    exit 1
+  else
+   synccmd="rclone --config "/config/.rclone.conf" sync /data $REMOTE_SYNC:/"
+   echo "Executing => $synccmd"
+   eval "$synccmd"
+  fi
+) 200>/var/lock/rclone.lock
