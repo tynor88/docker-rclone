1e98273c91081fd7480c03e8b4b588a48a75d57e
Thomas Tynor <tynor@hotmail.com>
2016-09-18 08:52:18 +0200

    try to sleep to test if flock works properly

--- root/app/rclone.sh
+++ root/app/rclone.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/with-contenv sh
 
 (
-  flock -xn 200
+  flock -n 200 || exit 1
+  
+  sleep 10
   
   if [ -z "$REMOTE_SYNC" ]; then
     echo "Error: REMOTE_SYNC environment variable was not passed to the container."
