c25c700f4cba326b191cbc1cd83555c284b3fee3
Thomas Tynor <tynor@hotmail.com>
2016-09-18 10:40:18 +0200

    add environment variable to add a subpath to

--- root/app/rclone.sh
+++ root/app/rclone.sh
@@ -3,12 +3,11 @@
 (
   flock -n 200 || exit 1
   
-  if [ -z "$REMOTE_SYNC" ]; then
-    echo "Error: REMOTE_SYNC environment variable was not passed to the container."
+  if [ -z "$SYNC_DESTINATION" ]; then
+    echo "Error: SYNC_DESTINATION environment variable was not passed to the container."
     exit 1
   else
-   synccmd="rclone --config "/config/.rclone.conf" sync /data $REMOTE_SYNC:/"
-   echo "Executing => $synccmd"
-   eval "$synccmd"
+    echo "Executing => $synccmd"
+    eval "$synccmd"
   fi
 ) 200>/var/lock/rclone.lock
