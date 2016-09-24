f1a6a43ae549457ebde47e6940ff2c25e706b74c
Thomas Tynor <tynor@hotmail.com>
2016-09-24 23:18:11 +0200

    Custom command possible

--- root/app/rclone.sh
+++ root/app/rclone.sh
@@ -3,12 +3,11 @@
 (
   flock -n 200 || exit 1
   
-  if [ -z "$SYNC_DESTINATION" ]; then
-    echo "Error: SYNC_DESTINATION environment variable was not passed to the container."
-    exit 1
   else
     synccmd="rclone sync /data $SYNC_DESTINATION:/$SYNC_DESTINATION_SUBPATH"
     echo "Executing => $synccmd"
     eval "$synccmd"
+    fi
   fi
+  
 ) 200>/var/lock/rclone.lock
