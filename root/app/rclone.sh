83568fd3024f58bd93f8bc454bd606c29ea5743e
Thomas Tynor <tynor@hotmail.com>
2017-01-30 20:51:17 +0100

    proper escaping
    according to rclone docs "If your names have spaces or shell
    metacharacters (eg *, ?, $, ', " etc) then you must quote them. Use
    single quotes ' by default."

--- root/app/rclone.sh
+++ root/app/rclone.sh
@@ -2,9 +2,9 @@
 
 (
   flock -n 200 || exit 1
-  
-  sync_command="rclone sync /data $SYNC_DESTINATION:/$SYNC_DESTINATION_SUBPATH"
-  
+
+  sync_command="rclone sync /data $SYNC_DESTINATION:/'$SYNC_DESTINATION_SUBPATH'"
+
   if [ "$SYNC_COMMAND" ]; then
   sync_command="$SYNC_COMMAND"
   else
@@ -13,7 +13,7 @@
       exit 1
     fi
   fi
-  
+
   echo "Executing => $sync_command"
   eval "$sync_command"
 ) 200>/var/lock/rclone.lock
